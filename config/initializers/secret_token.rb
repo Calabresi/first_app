f897b8b6d5189affaa57d22f5f90cfcd59cc4faf
Matthew <matthew@wpengine.com>
2013-12-09 13:17:33 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '8a1b1968b0d1ec4550c9ae8645dbca162dd882622ccdb1b4e78de030046a1e5a5ed8e30f60bdd0d238ad6f4bac6e87e36778dfc730dd0769e934fb1d9eb5c8bc'
